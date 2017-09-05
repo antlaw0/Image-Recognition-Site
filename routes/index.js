2b22c06ae26f693bad9cc3bd72f3d4facdcb1c70
AAA <aaa@aaa.com>
2017-09-05 16:59:52 -0500

    making sure have latest code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var clarifai = require('../helpers/image_recog');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Recognition' });
 });
+
+
+//handle POST request
+router.post('/upload', function(req, res, next){
+
+
+}); //end of post to Clarifai
 
 module.exports = router;
