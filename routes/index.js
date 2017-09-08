bf8958b906f9f3329ed10e81f55222ef144ddb72
AAA <aaa@aaa.com>
2017-09-08 17:48:03 -0500

    Trying to use temporary file

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var clarifai = require('../helpers/image_recog');
-
+var tmp = require('tmp');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  clarifai(function(data) {
+  clarifai('test file name', function(data) {
     console.log('This message is in index.js. The data received is ' + data );
   })
 
@@ -19,6 +20,26 @@ router.get('/', function(req, res, next) {
 
 //handle POST request
 router.post('/upload', function(req, res, next){
+
+tmp.file(function _tempFileCreated(err, path, fd, cleanupCallback) {
+  if (err) throw err;
+ 
+  console.log('File: ', path);
+  console.log('Filedescriptor: ', fd);
+  
+  // If we don't need the file anymore we could manually call the cleanupCallback 
+  // But that is not necessary if we didn't pass the keep option because the library 
+  // will clean after itself. 
+  cleanupCallback();
+
+  getImageDescription(_tempFileCreated)
+
+  });
+
+
+
+
+
 
 
 }); //end of post to Clarifai
